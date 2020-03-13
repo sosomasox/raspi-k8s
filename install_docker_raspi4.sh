7c2801cfc35b5229bf4a862d1b2fde660c4b08bf
相馬 大知 (Taichi Soma) <47720072+izewfktvy533zjmn@users.noreply.github.com>
2020-03-13 13:42:04 +0900

    Create install_docker_raspi4.sh

---
+++ install_docker_raspi4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $(whoami) != "root" ]; then
+    echo 'You are not root.'
+    echo 'You need to be root authority to execute.'
+    exit 1
+fi
+
+# if you added "cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1" to /boot/cmdline.txt, you can install docker below command
+curl -sSL https://get.docker.com | sh
+apt-mark hold docker-ce
+usermod -aG docker pi
+
+echo 'You need to reboot.'
+
+exit 0
