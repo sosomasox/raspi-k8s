54773476c44e3fc64cadde236bd6953065d17b5e
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-03-24 22:19:58 +0900

    fix

--- install_docker_for_raspi4.sh
+++ install_docker_for_raspi4.sh
@@ -18,7 +18,7 @@ wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/doc
 #wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce_18.06.3~ce~3-0~debian_armhf.deb
 #wget https://download.docker.com/linux/debian/dists/buster/pool/stable/armhf/docker-ce_18.09.9~3-0~debian-buster_armhf.deb
 
-sudo dpkg -i containerd.io_1.2.5-1_armhf.deb
+sudo dpkg -i containerd.io_1.2.6-3_armhf.deb 
 #sudo dpkg -i docker-ce-cli_18.09.0~3-0~debian-buster_armhf.deb 
 sudo dpkg -i docker-ce_18.06.1~ce~3-0~debian_armhf.deb 
 
