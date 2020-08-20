86ce300396809a0341e27c34cf8315fc4f6bd1fc
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-08-20 15:48:04 +0000

    Add scripts/kubernetes/v1.17.5/

---
+++ scripts/kubernetes/v1.17.5/ubuntu/install_kubernetes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+if [ $(whoami) != "root" ]; then
+    echo 'You are not root.'
+    echo 'You need to be root authority to execute.'
+    exit 1
+fi
+
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+apt install -y kubelet=1.17.5-00 kubeadm=1.17.5-00 kubectl=1.17.5-00 
+
+
+systemctl daemon-reload
+systemctl restart kubelet.service
+apt-mark hold kubeadm kubectl kubelet kubernetes-cni
+
+echo
+echo 'Done.'
+
+exit 0
